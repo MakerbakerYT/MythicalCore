dd3ef26e246acc9f54f7c9e675a13fbbdebcfdf2
MakerbakerYT <65385732+MakerbakerYT@users.noreply.github.com>
2020-10-13 00:54:54 -0600

    Update LeaderSubCommand.php

--- src/core/faction/command/subCommands/LeaderSubCommand.php
+++ src/core/faction/command/subCommands/LeaderSubCommand.php
@@ -6,7 +6,7 @@ namespace core\faction\command\subCommands;
 
 use core\command\utils\SubCommand;
 use core\faction\Faction;
-use core\CrypticPlayer;
+use core\MythicalPlayer;
 use core\translation\Translation;
 use core\translation\TranslationException;
 use pocketmine\command\CommandSender;
@@ -29,7 +29,7 @@ class LeaderSubCommand extends SubCommand {
      * @throws TranslationException
      */
     public function execute(CommandSender $sender, string $commandLabel, array $args): void {
-        if(!$sender instanceof CrypticPlayer) {
+        if(!$sender instanceof MythicalPlayer) {
             $sender->sendMessage(Translation::getMessage("noPermission"));
             return;
         }
